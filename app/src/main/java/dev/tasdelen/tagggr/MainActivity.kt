b7cf2401bb13e2649a4e160a577cdcabdffb2afc
Sergen Tasdelen <fb.sergen.tasdelen@gmail.com>
2019-11-10 02:23:27 +0100

    Login FB (FB SDK) Remplacé par Firebase AuthUI Login

--- app/src/main/java/dev/tasdelen/tagggr/MainActivity.kt
+++ app/src/main/java/dev/tasdelen/tagggr/MainActivity.kt
@@ -1,10 +1,7 @@
 package dev.tasdelen.tagggr
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.app.Activity
 import com.facebook.CallbackManager
-import com.google.firebase.auth.FirebaseAuth
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.FacebookException
@@ -18,47 +15,104 @@ import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import java.util.*
 import com.facebook.login.LoginManager
 import android.util.Log
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 
 class MainActivity : AppCompatActivity() {
 
     private var callbackManager: CallbackManager? = null
 
+    companion object {
+
+        private const val RC_SIGN_IN = 123
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createSignInIntent()
 
 
 
 
         // Login
-        callbackManager = CallbackManager.Factory.create()
-        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
-        LoginManager.getInstance().registerCallback(callbackManager,
-            object : FacebookCallback<LoginResult> {
-                override fun onSuccess(loginResult: LoginResult) {
-                    Log.d("letsSee", "Facebook token: " + loginResult.accessToken.token)
-
-                }
-
-                override fun onCancel() {
-                    Log.d("letsSee", "Facebook onCancel.")
-
-                }
-
-                override fun onError(error: FacebookException) {
-                    Log.d("letsSee", "Facebook onError.")
-
-                }
-            })
+//        callbackManager = CallbackManager.Factory.create()
+//        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
+//        LoginManager.getInstance().registerCallback(callbackManager,
+//            object : FacebookCallback<LoginResult> {
+//                override fun onSuccess(loginResult: LoginResult) {
+//                    Log.d("letsSee", "Facebook token: " + loginResult.accessToken.token)
+//
+//                }
+//
+//                override fun onCancel() {
+//                    Log.d("letsSee", "Facebook onCancel.")
+//
+//                }
+//
+//                override fun onError(error: FacebookException) {
+//                    Log.d("letsSee", "Facebook onError.")
+//
+//                }
+//            })
 
     }
+    private fun createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.FacebookBuilder().build())
 
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setIsSmartLockEnabled(false)
+                .setLogo(R.drawable.com_facebook_profile_picture_blank_portrait)
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+        // [END auth_fui_create_intent]
+    }
+
+    // [START auth_fui_result]
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        callbackManager?.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
 
-        Log.d("letsSee", "malsehnnnnnn: " + data)
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
+    // [END auth_fui_result]
+
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        callbackManager?.onActivityResult(requestCode, resultCode, data)
+//
+//        Log.d("letsSee", "malsehnnnnnn: " + data)
+//    }
 }
